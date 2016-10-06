15265f33f376c2d5373e46b61feab81389de6bd4
Arthur Julião <ArthurJ@users.noreply.github.com>
2016-10-06 14:52:02 -0300

    Update ranked_pair.py
    Adição de um método de ordenação (ainda comentado, precisa ser testado) que resolve empates entre maiorias comparando os vencedores empatados entre si na lista subsequente de forma recursiva.

--- ranked_pair.py
+++ ranked_pair.py
@@ -28,6 +28,62 @@ if __name__ == "__main__":
     '''
         Definir a lista de preferência sem as repetições, e em caso de empate entre maiorias do candidato A e B,
         usar a referência de A versus B para decidir quem tem a preferência.
+        
+        from operator import itemgetter
+#Testar
+# dicionario esperado = {(vencedor, perdedor): margem}
+# resultado esperado, valores ordenados por margem
+# em caso de empate, o desempate se deve pela ordem de maiores margens que forem menores que a de empate
+
+def ordenar(dicionario):
+    casos = [(x, y) for x, y in dicionario.items()]
+    maiorias = sorted(list(set([y for x, y in casos])), key=itemgetter(1), reverse=True)
+    ocorrencias = [0]*len(maiorias)
+    for i, maioria in enumerate(maiorias):    
+        ocorrencias[i] = sum([1 for x, y in casos if y == maioria])
+    casos = sorted(casos, key=itemgetter(1), reverse=True)
+    
+    resultado = []
+
+    candidatos_fixados = []
+    for i, maioria in enumerate(maiorias):
+        if  ocorrencias[i] == 1:
+            resultado.append([caso for caso in casos 
+                                if (caso[1] == maioria][0]) and caso[0][0] not in candidatos_fixados])
+            candidatos_fixados.append(caso[0][0])
+            
+        else:  # busque os vencedores empatados
+            candidatos_empatados = [caso[0][0] for caso in casos 
+                                        if (caso[1] == maioria) and caso[0][0] not in candidatos_fixados]
+            candidatos_empatados = list(set(candidatos_empatados))
+
+            casos_de_candidatos_empatados = [caso for caso in casos
+                                                if (caso[1] < maioria)
+                                                    and caso[0][0] in candidatos_empatados 
+                                                    and caso[0][1] in candidatos_empatados]
+            
+            # Se houver entre os casos de empate um caso em que ambos vitoriosos estejam com suas margens empatadas
+            casos_de_candidatos_empatados.append([caso for caso in casos 
+                                                    if (caso[1] == maioria][0]) 
+                                                    and caso[0][0] not in candidatos_fixados
+                                                    and caso[0][0] in candidatos_empatados 
+                                                    and caso[0][1] in candidatos_empatados])
+                                                    
+            subordem = ordenar(dict(casos_de_candidatos_empatados))  # ordene as combinações entre eles
+
+            for c in subordem:  # use essa ordenação para desempatar
+                resultado.append([caso for caso in casos 
+                                    if (caso[1] == maioria][0]) 
+                                    and c[0][0] == caso[0][0]
+                                    and caso[0][0] not in candidatos_fixados])
+                candidatos_fixados.append(c[0][0])
+
+    return resultado
+
+
+        # ------------------------------------------------------------------------------------------
+        # Outra forma possivel de desempate:
+        # pegue (na lista subsequente) o que tiver maior precedencia (sem empate) entre os empatados)
     '''
 
     for i in result:
